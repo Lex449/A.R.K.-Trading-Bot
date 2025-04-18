347751299d5259e1d88fe8b36c85622725a8e6d9
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-18 20:32:01 +0800

    Update error_handler.py

--- bot/utils/error_handler.py
+++ bot/utils/error_handler.py
@@ -1,7 +1,11 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+import logging
 
-async def handle_error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f"[FEHLER] {context.error}")
-    if update and update.message:
+logger = logging.getLogger(__name__)
+
+async def handle_error(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+    
+    if isinstance(update, Update) and update.message:
         await update.message.reply_text("Ein unerwarteter Fehler ist aufgetreten.")
