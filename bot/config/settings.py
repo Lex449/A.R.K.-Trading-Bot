6c1a0fd846d59f3d6d25b6c6c2efb7597e68a061
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-18 19:36:45 +0800

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,13 +1,9 @@
 import os
-from dotenv import import load_dotenv
 
-load_dotenv()
-
+# Liefert alle wichtigen Variablen aus Railway-Umgebung zurück
 def get_settings():
     return {
         "BOT_TOKEN": os.getenv("BOT_TOKEN"),
         "DANIEL_TELEGRAM_ID": os.getenv("DANIEL_TELEGRAM_ID"),
         "TWELVEDATA_API_KEY": os.getenv("TWELVEDATA_API_KEY")
     }
-
-# updated settings.py for deployment
