84d18545a0a4a710132078dcc4c95c87b8cad49d
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-18 18:47:12 +0800

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,1 +1,6 @@
-test
+def get_settings():
+    return {
+        "TELEGRAM_TOKEN": "7655719634:AAE01VP0eZP3gQGXgiL_hHOgBD3pG5yzId4",
+        "TELEGRAM_USER_ID": "7699862580",
+        "TWELVE_DATA_API_KEY": "0dd4ddf44b144ea48df01c9fdfc80921"
+    }
