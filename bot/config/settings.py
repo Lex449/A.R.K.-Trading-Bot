da6d42ec9f3adf9699ad8a80630d60cb7120cf81
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-19 16:47:13 +0800

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -1,13 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# .env-Datei laden
+# Lädt die .env-Datei
 load_dotenv()
 
 def get_settings():
-    """Lädt alle sensiblen Einstellungen aus der .env-Datei"""
     return {
         "TOKEN": os.getenv("BOT_TOKEN"),
         "DANIEL_ID": os.getenv("DANIEL_TELEGRAM_ID"),
-        "TWELVEDATA_KEY": os.getenv("TWELVEDATA_API_KEY"),
+        "TWELVEDATA_KEY": os.getenv("TWELVEDATA_API_KEY")
     }
