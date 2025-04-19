dcd7b6cd6cd21583928efd6bf83b40273b8c830a
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-19 16:59:21 +0800

    Update settings.py

--- bot/config/settings.py
+++ bot/config/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def get_settings():
     return {
-        "TOKEN": os.getenv("BOT_TOKEN"),
-        "DANIEL_ID": os.getenv("DANIEL_TELEGRAM_ID"),
-        "TWELVEDATA_KEY": os.getenv("TWELVEDATA_API_KEY")
+        "TOKEN": os.getenv("7655719634:AAE01VP0eZP3gQGXgiL_hHOgBD3pG5yzId4"),
+        "DANIEL_ID": os.getenv("7699862580"),
+        "TWELVEDATA_KEY": os.getenv("0dd4ddf44b144ea48df01c9fdfc80921")
     }
