0aae3529d67c862401691a79f559c5707bdf7ed8
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-23 13:29:16 +0800

    Update language.py

--- bot/utils/language.py
+++ bot/utils/language.py
@@ -1,11 +1,10 @@
-# bot/utils/language.py
-
 from telegram import Update
 
 def get_language(update: Update) -> str:
     """Ermittelt die Sprache des Benutzers (Deutsch oder Englisch)."""
     lang = update.message.from_user.language_code
 
+    # Falls die Sprache Deutsch oder Englisch ist, gebe die Sprache zurück, ansonsten Englisch als Standard.
     if lang in ['de', 'en']:
         return lang
     return 'en'  # Standard ist Englisch, wenn eine unbekannte Sprache erkannt wird.
