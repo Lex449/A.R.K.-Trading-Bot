d3fa2a4f4a2b78576ed644b79d284f8a94f75901
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-24 21:07:47 +0800

    Update finnhub_client.py

--- bot/engine/finnhub_client.py
+++ bot/engine/finnhub_client.py
@@ -1,10 +1,15 @@
-# bot/engine/finnhub_client.py
 import os
 import finnhub
 from dotenv import load_dotenv
 
+# .env Variablen laden
 load_dotenv()
 FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
 
+# Validierung
+if not FINNHUB_API_KEY:
+    raise ValueError("❌ FINNHUB_API_KEY fehlt! Bitte in .env oder Railway eintragen.")
+
+# Rückgabe eines neuen Finnhub Clients bei jedem Aufruf
 def get_finnhub_client():
     return finnhub.Client(api_key=FINNHUB_API_KEY)
