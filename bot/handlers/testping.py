e889c27ffcde09ab439de6343fdb7a24d9a181d0
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-24 17:12:44 +0800

    Delete bot/handlers/testping.py

--- bot/handlers/testping.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes, CommandHandler
-
-async def testping(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("📡 Test-Ping erfolgreich. A.R.K. antwortet.")
-
-testping_handler = CommandHandler("testping", testping)
