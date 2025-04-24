51c022b716a7f4ccd2dcd1c5b8d79e1d325c71c7
Lex449 <107636988+Lex449@users.noreply.github.com>
2025-04-25 00:00:30 +0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,14 +1,21 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()  # lädt Variablen aus .env
 
-FINNHUB_API_KEY     = os.getenv("FINNHUB_API_KEY", "")
-TELEGRAM_BOT_TOKEN  = os.getenv("TELEGRAM_BOT_TOKEN", "")
-TELEGRAM_CHAT_ID    = os.getenv("TELEGRAM_CHAT_ID", "")
-INTERVAL            = os.getenv("INTERVAL", "1h")  # Kerzenintervall, z.B. 1h, 1d
-RSI_PERIOD          = int(os.getenv("RSI_PERIOD", "14"))
-EMA_SHORT_PERIOD    = int(os.getenv("EMA_SHORT_PERIOD", "12"))
-EMA_LONG_PERIOD     = int(os.getenv("EMA_LONG_PERIOD", "26"))
-SIGNAL_CHECK_INTERVAL = int(os.getenv("SIGNAL_CHECK_INTERVAL_SEC", "60"))
-MAX_SIGNALS_PER_HOUR  = int(os.getenv("MAX_SIGNALS_PER_HOUR", "3"))
-AUTO_SIGNAL_SYMBOLS = os.getenv("AUTO_SIGNAL_SYMBOLS", "US100,US30,DE40,JP225,HK50,SPX500").split(",")
+load_dotenv()  # Lädt alle Umgebungsvariablen
+
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY", "")
+TELEGRAM_BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+
+# Analyseparameter – feinjustiert für Hochfrequenz-Scans
+INTERVAL = os.getenv("INTERVAL", "1")  # Minutenintervall: 1 = 1m
+RSI_PERIOD = int(os.getenv("RSI_PERIOD", "14"))
+EMA_SHORT_PERIOD = int(os.getenv("EMA_SHORT_PERIOD", "9"))
+EMA_LONG_PERIOD = int(os.getenv("EMA_LONG_PERIOD", "21"))
+
+# Signal-Konfiguration für maximalen Output
+SIGNAL_CHECK_INTERVAL = int(os.getenv("SIGNAL_CHECK_INTERVAL_SEC", "60"))  # Wie oft gescannt wird (Sek.)
+MAX_SIGNALS_PER_HOUR = int(os.getenv("MAX_SIGNALS_PER_HOUR", "9000"))  # Maximal mögliche Signale pro Stunde
+
+# Fokus auf 7 US-Märkte mit hoher Volatilität & Liquidität
+AUTO_SIGNAL_SYMBOLS = os.getenv("AUTO_SIGNAL_SYMBOLS", "US100,US30,SPX500,IWM,QQQ,DIA,MDY").split(",")
